b7b365dabed3c8ed8396c4d4f88b11226258177a
Ubuntu <hack@yomiuri-hackathon.nznxow5pasxexjrzw13qnli2og.lx.internal.cloudapp.net>
2017-05-21 02:17:48 +0000

    21日昼時点の成果

--- html/js/TsunamiBousai.js
+++ html/js/TsunamiBousai.js
@@ -3,70 +3,79 @@
 	var w = $(window).width();
 	var h = $(window).height();
 
+  var slideNum = 8;
+
   var mapWidth=1000;
   var mapHeight=1000;
 
   document.write(
 		'<div class="content" style="width:'+(w)+'px;height:'+(h-0)+'px;">'+
-		'<div class="graph2016" style="width:'+mapWidth+'px;height:'+mapWidth+'px"><img src="./images/map1.jpg" style="width:'+mapWidth+'px;height:'+mapWidth+'px">'
+		'<div class="contentInner" style="width:'+mapWidth+'px;height:'+mapWidth+'px"><img src="./images/map1.jpg" style="width:'+mapWidth+'px;height:'+mapWidth+'px">'
 	);
   document.write(
 		'</div></div>'+
-			'<div class="pre"></div>'+
-			'<div class="after"></div>'
+			'<header></header>'+
+			'<div class="plus"></div>'+
+			'<div class="pre"><span>20110312</span></div>'+
+			'<div class="after"><span>20170414</span></div>'+
+			'<img src="./images/arw.png" class="arw">'
 	);
 /*  alert (((mapWidth-w)/2) + 'px');*/
+
+
+
+
   $(window).load(function(){
+    $('.carousel,.carousel .viewport,.carousel a,.carousel .item')
+      .css({width:w+'px'});
+
+    $('.carousel .flipsnap')
+        .css({width:(w * slideNum)+'px'});
+
+    Flipsnap('.carousel .flipsnap', {
+      /*distance: 230*/
+    });
+
 		$(".content").mCustomScrollbar({
 			axis:"yx",
 			scrollbarPosition:'inside',
 			autoHideScrollbar:true,
 			setLeft: ((mapWidth-w)/2) + 'px',
-      setTop: '100px'
+      setTop: ((mapHeight-h)/2) + 'px'
       ,
       callbacks:{
-        onScroll:function(){
-          console.log('Content scrolled...');
+        whileScrolling:function(){
+          /*console.log(this.mcs.top);*/
+
+          var offset = {
+            x : mapWidth/2 + this.mcs.left - w/2,
+            y : mapHeight/2 + this.mcs.top - h/2
+          }
+          //console.log($('.content').find('.pre').length);
+          $('.pre,.after')
+            .css({
+              'background-position-x': (-190 + offset.x*2 )+'px',
+              'background-position-y': (-190 + offset.y*2 )+'px',
+              'background-size': '500px'
+            })
+            ;
+
+
+
+
+
+
+/*
+          console.log('w:'+(mapWidth/2+this.mcs.left - w/2));
+          console.log('h:'+(mapHeight/2+this.mcs.top - h/2));
+          console.log('h:'+this.mcs.top+' '+mapHeight+' '+h + ' w:'+ this.mcs.left+' '+mapWidth+' '+w);
+*/
+          //h:0 1000 667 w:0 1000 375
+          //h:-363 1000 667 w:-655 1000 375
+
         }
       }
-
-
 			/*setTop: ((mapHeight-h)/2) + 'px'*/
 		});
 	});
 })();
-
-/*
-(function (){
-	var w = $(window).width();
-	var h = $(window).height();
-
-	document.write(
-		'<div class="hakoneResultsGraph" style="width:100%;height:'+(h-10)+'px;">'+
-		'<div class="graph2016" style="width:630px">'
-	);
-	new HakoneResults({
-		isSp : true,
-		year: 2016,
-		width: 450,
-		mode : 'xml',
-		PCprofPath : 'http://www.yomiuri.co.jp/sports/ekiden/2016/team/profile/',
-		SPprofPath : 'http://sp.yomiuri.co.jp/sports/ekiden/2016/team/profile/',
-		univLen : 21,
-		univHStep : 27,
-		univNameButtonW : 60,
-		hideLatestTsusanTime : false
-	});
-
-	document.write(
-		'</div></div>'
-	);
-	$(window).load(function(){
-		$(".hakoneResultsGraph").mCustomScrollbar({
-			axis:"yx",
-			setLeft: (w - 590) + 'px',
-			setTop: 0
-		});
-	});
-})();
-*/
